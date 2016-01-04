36a258658e3129f963e85381d26947e870261976
vgeirnaert <gsfjohndoe@hotmail.com>
2016-01-04 13:16:28 +0100

    Adding cachebuster to force a reload of css file

--- BetterWEP.user.js
+++ BetterWEP.user.js
@@ -8,7 +8,7 @@
 // @grant        GM_addStyle
 // @grant        GM_getResourceText
 // @require      http://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js
-// @resource     betterWEP_css https://raw.githubusercontent.com/vgeirnaert/BetterWEP/master/BetterWEP.css 
+// @resource     betterWEP_css https://raw.githubusercontent.com/vgeirnaert/BetterWEP/master/BetterWEP.css?cache=1
 // ==/UserScript==
 
 function betterWep() {
